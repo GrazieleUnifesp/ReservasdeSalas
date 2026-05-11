4969d0c2b9edc7fcf46cdcf95bcd6a207babebbe
Graziele Sales <grazielesalesb@outlook.com>
2026-05-11 16:54:18 -0300

    implementa Decorator para reserva com multimídia (bônus)

---
+++ src/com/roombooking/decorator/ComMultimidia.java
@@ -0,0 +1,22 @@
+package com.roombooking.decorator;
+
+import com.roombooking.model.Reserva;
+
+// Commit 18 - Ana Clara
+// Decorator concreto: adiciona equipamento multimídia à reserva
+public class ComMultimidia extends ReservaDecorator {
+
+    public ComMultimidia(Reserva reserva) {
+        super(reserva);
+    }
+
+    @Override
+    public String getDescricaoExtras() {
+        return "Equipamento Multimídia (projetor + caixas de som)";
+    }
+
+    @Override
+    public double getCustoExtra() {
+        return 50.0;
+    }
+}
