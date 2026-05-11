864b489b186bd96206c11beb98d7011eb0a31f27
Graziele Sales <grazielesalesb@outlook.com>
2026-05-11 09:43:06 -0300

    feat: adiciona hierarquia de usuários (Estudante e Professor)

---
+++ Estudante.java
@@ -0,0 +1,16 @@
+package com.roombooking.model;
+
+// Commit 2 - Graziele
+public class Estudante extends Usuario {
+    private String matricula;
+
+    public Estudante(String id, String nome, String email, String matricula) {
+        super(id, nome, email);
+        this.matricula = matricula;
+    }
+
+    public String getMatricula() { return matricula; }
+
+    @Override
+    public String getTipo() { return "Estudante"; }
+}
